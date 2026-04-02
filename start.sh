d5fa8def9fff223478f5d0c1096e597b8fec47da
Nuriddin Sobirjonov <sobirjonovnuriddin43@gmail.com>
2026-04-02 16:51:18 +0500

    don

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 ./wait-for-it.sh postgres.railway.internal 5432 -- \
 # Wait for Redis
 ./wait-for-it.sh redis.railway.internal 6379 -- \
-npx prisma migrate deploy && \
+npx prisma db push && \
 node -r tsconfig-paths/register dist/src/main.js
 
